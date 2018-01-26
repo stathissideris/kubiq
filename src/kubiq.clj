3bf4dbd34e4371ae62f4220edec462c1117a2a3d
Stathis Sideris <stathis@pixelated-noise.com>
2018-01-26 17:38:00 +0200

    Handle JavaFX init for compilation

--- src/kubiq.clj
+++ src/kubiq.clj
@@ -2,7 +2,12 @@
 
 ;;namespace of aliasing purposes
 
+(def force-toolkit-init-for-compilation
+  (when *compile-files*
+    (println "Initializing JavaFX for compilation...")
+    (javafx.embed.swing.JFXPanel.)))
+
 (def force-toolkit-init
   (when-not *compile-files*
-    (println "Initializing JavaFX")
+    (println "Initializing JavaFX...")
     (javafx.embed.swing.JFXPanel.)))
